ff49eabdd2018d9c958963e913ddde0ea656da02
jieun-git <forworkje21@gmail.com>
2025-02-05 11:39:27 +0900

    Wip: 반응형 스타일 테스트

--- Desktop/Project/carrot-market-reloaded/app/page.tsx
+++ Desktop/Project/carrot-market-reloaded/app/page.tsx
@@ -1,32 +1,26 @@
 export default function Home() {
   return (
-    <main className='bg-gray-300 h-screen flex items-center justify-center p-5'>
-      <div className='bg-white shadow-lg p-5 rounded-2xl w-full'>
-        <div className='flex items-center justify-between'>
-          <div className='flex flex-col'>
-            <span className='text-gray-600 font-semibold -mb-1'>
-              In transit
-            </span>
-            <span className='text-4xl font-semibold'>Coolblue</span>
-          </div>
-          <div className='size-12 rounded-full  bg-orange-400' />
-        </div>
-        <div className='my-2 flex items-center gap-2'>
-          <span className='bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full'>
-            TODAY
-          </span>
-          <span>9:30-10:30</span>
-        </div>
-        <div className='relative'>
-          <div className='bg-gray-200 rounded-full w-full h-2 absolute' />
-          <div className='bg-green-400 rounded-full w-2/3 h-2 absolute' />
-        </div>
-        <div className='flex justify-between items-center mt-5 text-gray-600'>
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className='text-gray-400'>Delivered</span>
-        </div>
+    <main className='bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5'>
+      <div
+        className='bg-white shadow-lg p-5
+                      rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2'
+      >
+        <input
+          className='w-full
+                       rounded-full h-12 bg-gray-200 pl-5 outline-none ring
+      ring-transparent
+      focus:ring-orange-500 focus:ring-offset-2
+      transition-shadow placeholder:drop-shadow'
+          type='text'
+          placeholder='Search here...'
+        />
+        <button
+          className='bg-black text-white py-2
+      rounded-full
+    active:scale-90 transition-transform font-medium md:px-5 outline-none'
+        >
+          Search
+        </button>
       </div>
     </main>
   )
