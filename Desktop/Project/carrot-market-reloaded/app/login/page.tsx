62b7fe3ceb90770c7ca9e0d1cd81ac2e3dfc8f8c
jieun-git <forworkje21@gmail.com>
2025-02-07 14:32:02 +0900

    Feat: 로그인, 회원가입 UI 구현

---
+++ Desktop/Project/carrot-market-reloaded/app/login/page.tsx
@@ -0,0 +1,28 @@
+import FormInput from '@/components/form-input'
+import FormButton from '@/components/form-btn'
+import SocialLogin from '@/components/social-login'
+
+export default function Login() {
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl ">안녕하세요!</h1>
+        <h2 className="text-xl">Log in with email and password.</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput
+          type="email"
+          placeholder="Email"
+          errors={[]}
+        />
+        <FormInput
+          type="password"
+          placeholder="Password"
+          errors={[]}
+        />
+        <FormButton loading={false} text="로그인"/>
+      </form>
+      <SocialLogin />
+    </div>
+  )
+}
