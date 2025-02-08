0d70e84ad6d0d67d4fe6293c50a05ab9ee0cb4f7
jieun-git <forworkje21@gmail.com>
2025-02-08 14:16:29 +0900

    Feat: commit update

---
+++ app/login/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import FormInput from "@/components/form-input";
+import FormButton from "@/components/form-btn";
+import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { handleForm } from "@/app/login/action";
+
+export default function Login() {
+  const [state, action] = useFormState(handleForm, null);
+
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl ">안녕하세요!</h1>
+        <h2 className="text-xl">Log in with email and password.</h2>
+      </div>
+      <form className="flex flex-col gap-3" action={action}>
+        <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          errors={[]}
+          required
+        />
+        <FormInput
+          name="password"
+          type="password"
+          placeholder="Password"
+          errors={state?.errors ?? []}
+          required
+        />
+        <FormButton text="로그인" />
+      </form>
+      <SocialLogin />
+    </div>
+  );
+}
---
+++ components/form-btn.tsx
@@ -0,0 +1,26 @@
+// useFormStatus
+// 훅은 React 에서 기본으로 제공해주는 form action 관련 훅
+// 반드시 form 의 자식 컴포넌트 내부에서만 호출이 됨. form 이 있는 곳에서 호출되지 않음
+
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+interface FormButtonProps {
+  text: string;
+}
+
+export default function FormButton({ text }: FormButtonProps) {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      disabled={pending}
+      className="primary-btn h-10
+      disabled:bg-neutral-400
+      disabled:text-neutral-300 disabled:cursor-not-allowed"
+    >
+      {pending ? "로딩 중..." : text}
+    </button>
+  );
+}
