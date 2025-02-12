9850c87025e2dfb9da5a2831c1d347346364faff
jieun-git <forworkje21@gmail.com>
2025-02-12 12:58:32 +0900

    Feat: create account cookie 연동

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -5,6 +5,11 @@ import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import db from "@/lib/db";
+import bcrypt from "bcrypt";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const checkUsername = (username: string) => !username.includes("potato");
 const checkPassword = ({
@@ -14,6 +19,32 @@ const checkPassword = ({
   password: string;
   confirm_password: string;
 }) => confirm_password === password;
+
+const checkUniqueUsername = async (username: string) => {
+    const user = await db.user.findUnique({
+        where: {
+            username: username
+        },
+        select: {
+            id: true
+        }
+    });
+    // already exist username ? false : true
+    return !Boolean(user);
+}
+
+const checkUniqueEmail = async (email: string) => {
+    const user = await db.user.findUnique({
+        where: {
+            email: email,
+        },
+        select: {
+            id: true
+        }
+    })
+    // already exist email ? false : true
+    return !Boolean(user);
+}
 
 // 유효성 조건
 const formScheme = z
@@ -27,13 +58,13 @@ const formScheme = z
       .max(10, "Way too long!!")
       .toLowerCase()
       .trim()
-      .transform((username) => `🔥 ${username} 🔥`)
-      .refine(checkUsername, "No potato allowed!"),
-    email: z.string().email().trim().toLowerCase(),
+      // .transform((username) => `🔥 ${username} 🔥`)
+      .refine(checkUsername, "No potato allowed!").refine(checkUniqueUsername, "This username is already taken."),
+    email: z.string().email().trim().toLowerCase().refine(checkUniqueEmail, "There is an account already registered with that email."),
     password: z
       .string()
-      .min(PASSWORD_MIN_LENGTH)
-      .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+      .min(PASSWORD_MIN_LENGTH),
+      // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
   })
   .refine(checkPassword, {
@@ -42,18 +73,41 @@ const formScheme = z
   });
 
 const createAccount = async (prevState: any, formData: FormData) => {
-  const data = {
+    const data = {
     username: formData.get("username"),
     email: formData.get("email"),
     password: formData.get("password"),
     confirm_password: formData.get("confirm_password"),
   };
 
-  const result = formScheme.safeParse(data);
+  const result = await formScheme.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+      /* after zod validation */
+
+      const hashedPassword = await bcrypt.hash(result.data.password, 12)
+
+      const user = await db.user.create({
+          data: {
+              username: result.data.username,
+              email: result.data.email,
+              password: hashedPassword
+          },
+          select: {
+              id: true
+          }
+      })
+
+      const cookiesStore = await cookies()
+
+      const cookie = await getIronSession(cookiesStore, {
+          cookieName: 'delicious-carrot',
+          password: process.env.COOKIE_PASSWORD!,
+      })
+      cookie.id = user.id
+      await cookie.save()
+      redirect('/profile')
   }
 };
 
