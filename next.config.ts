efb77bf3459368f160e339718f5bab7569927437
jieun-git <forworkje21@gmail.com>
2025-02-18 17:34:31 +0900

    Feat: 최적화할 깃허브 이미지 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        // Image 컴포넌트에게 최적화할 이미지의 도메인 주소
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
+/*
+* NextJS의 Image는 이미지를 자동으로 최적화를 해 주어 성능을 향상시키고 빠른 로딩이 되도록 해 준다.
+하지만 외부 호스트의 이미지(다른 사이트의 이미지 링크 등)를 불러올 때는 보안 상의 이유로 이 기능이 허용되지 않는다.
+따라서 next.config.mjs에서 hostname들을 등록해 주어야 한다.
+*
+* */
