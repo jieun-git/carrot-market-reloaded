73a694b127d6814e3c0becd69bd30b913ff0b430
jieun-git <forworkje21@gmail.com>
2025-02-23 15:08:13 +0900

    Feat: Default route 페이지 추가

---
+++ app/(tabs)/home/@modal/default.tsx
@@ -0,0 +1,14 @@
+/* Parallel Routes
+ * 동시에 여러 페이지를 렌더링하거나 특정 조건에 따라 다르게 렌더링할 수 있도록 하는 기능
+ * 병렬 렌더링
+ * @폴더명 형식으로 정의
+ * */
+
+/* Default Route
+ * 초기 로드 또는 전체 페이지 새로고침 시, Parallel 라우팅에 대한 유효한 경로를 찾지 못했을 때 기본으로 보여주는 페이지
+ * 파일명은 반드시 default.tsx
+ * */
+
+export default function Default() {
+  return null;
+}
