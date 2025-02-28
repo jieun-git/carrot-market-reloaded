df990177540109ab415beb2e67077daa64524a6c
jieun-git <forworkje21@gmail.com>
2025-02-28 11:29:36 +0900

    Feat : Post, likes & dislikes UI 구현

--- app/(tabs)/life/page.tsx
+++ app/(tabs)/life/page.tsx
@@ -1,7 +1,68 @@
-export default function Life() {
-    return (
-        <div>
-            <h1 className="text-white text-4xl">Life!</h1>
-        </div>
-    )
+import db from "@/lib/db";
+import Link from "next/link";
+import { formatToTimeAgo } from "@/lib/utils";
+import {
+  ChatBubbleBottomCenterIcon,
+  HandThumbUpIcon,
+} from "@heroicons/react/24/outline";
+
+async function getPosts() {
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+      _count: {
+        // _count: Prisma 에서 Aggregation(집계) 작업을 수행할 때 사용하는 특수한 필드로 연관된 데이터의 개수를 구할 수 있음
+        select: {
+          comments: true,
+          likes: true,
+        },
+      },
+    },
+  });
+
+  return posts;
+}
+
+export const metadata = {
+  title: "동네생활",
+};
+
+export default async function Life() {
+  const posts = await getPosts();
+  console.log(posts);
+  return (
+    <div className="p-5 flex flex-col">
+      {posts.map((post) => (
+        <Link
+          key={post.id}
+          href={`/posts/${post.id}`}
+          className="pb-5 mb-5 border-b border-neutral-500 text-neutral-400 flex flex-col gap-2 last:pb-0 last:border-b-0"
+        >
+          <h2 className="text-white text-lg font-semibold">{post.title}</h2>
+          <p className="mb-2">{post.description}</p>
+          <div className="flex items-center justify-between text-sm">
+            <div className="flex gap-4 items-center ">
+              <span>{formatToTimeAgo(post.created_at.toString())}</span>
+              <span>.</span>
+              <span>조회 {post.views}</span>
+            </div>
+            <div className="flex gap-4 items-center *:flex *:gap-1 *:items-center">
+              <span>
+                <HandThumbUpIcon className="size-4" />
+                {post._count.likes}
+              </span>
+              <span>
+                <ChatBubbleBottomCenterIcon className="size-4" />
+                {post._count.comments}
+              </span>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
