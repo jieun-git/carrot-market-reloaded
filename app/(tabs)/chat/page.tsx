2f9e483dddda9b00ebee35ed3d0b08ae70aef865
jieun-git <forworkje21@gmail.com>
2025-03-03 15:03:51 +0900

    Feat: ChatRoom UI 구현

--- app/(tabs)/chat/page.tsx
+++ app/(tabs)/chat/page.tsx
@@ -1,7 +1,65 @@
-export default function Chat() {
-    return (
-        <div>
-            <h1 className="text-white text-4xl">Chat!</h1>
-        </div>
-    )
-}
+import db from "@/lib/db";
+import { unstable_cache as nextCache } from "next/cache";
+import getSession from "@/lib/session";
+import ChatRoomList from "@/components/chat-room-list";
+
+const getChats = async (userId: number) => {
+  const chats = await db.chatRoom.findMany({
+    where: {
+      users: {
+        some: {
+          id: userId,
+        },
+      },
+    },
+    select: {
+      id: true,
+      messages: {
+        take: 1,
+        orderBy: {
+          created_at: "desc",
+        },
+      },
+      users: {
+        where: {
+          id: {
+            not: userId,
+          },
+        },
+        select: {
+          avatar: true,
+          username: true,
+        },
+      },
+    },
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+  return chats;
+};
+
+const getCachedChats = nextCache(getChats, ["chat-list"], {
+  tags: ["chat-list"],
+});
+
+const Chat = async () => {
+  const session = await getSession();
+  const chats = await getCachedChats(session.id!);
+  return (
+    <div>
+      <h1 className="text-white text-4xl">Chats</h1>
+      {chats.map((chat) => (
+        <ChatRoomList
+          key={chat.id}
+          id={chat.id}
+          users={chat.users}
+          messages={chat.messages}
+          userId={session.id!}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chat;
---
+++ components/chat-room-list.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import Image from "next/image";
+import { UserIcon } from "@heroicons/react/24/solid";
+
+interface UsersProps {
+  username: string;
+  avatar?: string;
+}
+
+interface MessageProps {
+  id: number;
+  payload: string;
+  created_at: Date;
+  updated_at: Date;
+  chatRoomId: string;
+  userId: number;
+  isRead: boolean;
+}
+
+interface ChatRoomListProps {
+  id: string;
+  users: UsersProps[];
+  messages: MessageProps[];
+  userId: number;
+}
+
+const ChatRoomList = ({ id, users, messages, userId }: ChatRoomListProps) => {
+  const user = users[0];
+  if (!user) return;
+
+  const message = messages[0];
+
+  return (
+    <Link
+      href={`/chats/${id}`}
+      className="flex gap-5 p-5 items-center hover:opacity-80
+      transition-opacity"
+    >
+      {user.avatar ? (
+        <Image
+          src={user.avatar || "/carrot.jpeg"}
+          alt={user.username}
+          width={50}
+          height={50}
+          className="rounded-md"
+        />
+      ) : (
+        <UserIcon className="size-14 text-white" />
+      )}
+      <div>
+        <h2 className="text-lg text-white">{user.username}</h2>
+        {message && (
+          <>
+            <h3 className="text-sm text-neutral-500">
+              <span className="text-neutral-400">
+                {message.userId === userId ? "나" : user.username}
+              </span>
+              {message.payload}
+            </h3>
+          </>
+        )}
+      </div>
+      {message && !message.isRead && message.userId !== userId && (
+        <div className="size-2 rounded-full bg-white ml-auto" />
+      )}
+    </Link>
+  );
+};
+
+export default ChatRoomList;
